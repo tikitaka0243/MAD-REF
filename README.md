7dd3fb5f1afb185e0ef78269f98cf80ba2f75872
Leroy <165038371+Leroy-Xiong@users.noreply.github.com>
2025-04-11 20:38:02 +0800

    update

--- README.md
+++ README.md
@@ -1,17 +1,7 @@
-<!-- <span style="color: grey;">_It is better to read in light appearance._</span> -->
-
-<div align="right">
-  
-<span style="font-size: 12px; font-style: italic; opacity: 0.5;">
-  
-  _It is better to read in light appearance._
-  
-</span>
-
-</div>
 
 
-# Reconstructing and forecasting marine dynamic variable fields across space and time globally and gaplessly
+
+# From Sparse Spatio-temporal Data to Dynamic Ocean Fields: A Physics-Guided Neural Framework for Global Marine Predictions
 Zhixi Xiong, Yukang Jiang, Wenfang Lu, Xueqin Wang, and Ting Tian
 
 
@@ -19,108 +9,37 @@ Zhixi Xiong, Yukang Jiang, Wenfang Lu, Xueqin Wang, and Ting Tian
 
 
 
-# Abstract
+## Abstract
 
-Spatiotemporal projections in marine science are essential for understanding ocean systems and their impact on Earth's climate. 
-However, existing AI-based and statistics-based inversion methods face challenges in leveraging ocean data, generating continuous outputs, and incorporating physical constraints. 
-We propose the Marine Dynamic Reconstruction and Forecast Neural Networks (MDRF-Net), which integrates marine physical mechanisms and observed data to reconstruct and forecast continuous ocean temperature-salinity and dynamic fields.
-MDRF-Net leverages statistical theories and techniques, incorporating parallel neural network sharing initial layer, two-step training strategy, and ensemble methodology, facilitating in exploring challenging marine areas like the Arctic zone. 
-We have also theoretically justified the rationality of it by providing an upper bound on its generalization error.
-The effectiveness of MDRF-Net's is validated through a comprehensive simulation study, which highlights its capability to reliably estimate unknown parameters.
-Comparison with other inversion methods and reanalysis data are also conducted, and the global test RMSE is 0.455°C for temperature and 0.0714psu for salinity.
-Bridging the gap between statistics and AI, MDRF-Net effectively learns the ocean dynamics system using physical mechanisms and statistical insights, contributing to a deeper understanding of marine systems and their impact on the environment and human use of the ocean.
+The reconstruction of continuous ocean temperature-salinity and dynamic fields from sparse, heterogeneous observations is a critical challenge in marine science, with profound implications for climate prediction, resource management, and biodiversity conservation. 
+Despite recent advances, existing methods struggle to reconcile sparse observations with physical constraints, particularly in underobserved regions like the deep ocean and polar zones.
+To address these problems, we propose the Marine Dynamic Reconstruction and Forecast Neural Networks (MAD-REF Net), which integrates marine physical mechanisms and observed data to reconstruct and forecast continuous ocean temperature-salinity and dynamic fields.
+Notably, MAD-REF Net   is capable of reconstructing variables in regions lacking observational data, such as polar areas, and can infer unobserved variables, thereby providing a comprehensive understanding of marine dynamics across both data-rich and data-sparse regions.
+MAD-REF Net   leverages statistical theories and techniques to eliminate polar singularities, ensure global continuity, model multi-scale features, and enhance training efficiency while balancing data-physics alignment.
+Overall, MAD-REF Net   effectively learns the ocean dynamics system using physical mechanisms and statistical insights, contributing to a deeper understanding of marine systems and their impact on the environment and human use of the ocean.
 
 
-## Keywords
+### Keywords
 
 Fields inversion | Global ocean dynamics | Primitive equations | Uncollected marine variables
 
 
-# Marine Dynamic Reconstruction and Forecast Neural Networks (MDRF-Net)
-![MDRF-Net](/Image/MDRF-NetStructure.jpg)
+## Marine Dynamic Reconstruction and Forecast Neural Networks (MAD-REF Net)
+![MDRF-Net](/Image/MAD-REF_structure.jpg)
 
 
-# Simulation Study
-
-We first explore the capabilities of MDRF-Net in a simulated system by considering a 2D simplified version of the primitive equations which has only one dimension in the horizontal direction ($x$) and does not include the diffusion equation for salinity as well as the equation of state. The simplified equations involve four variables: temperature ($\tau$), horizontal velocity ($v$), vertical velocity ($w$), and pressure ($p$). The domain is based on a Cartesian coordinate system and is dimensionless.
-
-The simplified 2D primitive equations are given by:
-<p align="center">
-  <img src="Image/PEs.png" alt="Simulation equations" width="350"/>
-</p>
-
-where this system admits a specific Taylor-Green vortex solution corresponding to a periodic source term $Q$.
-
-The Taylor-Green vortex solution is as follows:
-<p align="center">
-  <img src="Image/Sol.png" alt="Simulation solution" width="500"/>
-</p>
-
-The results are presented below, with further details available in our paper.
-
-![Simulation study](/Image/simulation_comparison.jpg)
-
-Run the simulation study:
-```
-python Simulation.py
-```
-
-# Work with Real Data
-
-## Data source and processing
+## Real Data Source
 
 We utilized ocean temperature and salinity data from the Argo project (https://argo.ucsd.edu/) and current reanalysis data from the EU Copernicus ocean service (https://www.copernicus.eu/en).
 
 We found that a convenient way to download Argo data is to use Argo's data visualization tool, EuroArgo Selection Tool (https://dataselection.euro-argo.eu/), and select the CSV format. For the EU Copernicus marine service, it is better to use FileZilla to connect to their server and download the relevant NC data files (see Copernicus website for details), which is faster. The dataset we use is 'GLOBAL_ANALYSISFORECAST_PHY_001_024', and the codes 'cur' and 'wcur' in them represent the 3D ocean current reanalysis data.
 
-After downloading the data, run the `LoadData.py` file to process them.
-
+## Train the Model
 ```
-python LoadData.py --argo_data_path Data/Argo/RawFiles --argo_save_path Data/Argo
---currents_data_path Data/Currents/RawFiles --currents_save_path Data/Currents
+python Simulation.py
 ```
 
-Command-line arguments explained:
-* `argo_data_path` and `argo_save_path`: Path of the raw Argo data and path to save the processed Argo data.
-* `currents_data_path` and `currents_save_path`: Path of the raw currents data and path to save the processed currents data.
-* `r_min` and `r_max`: The minimum and maximum values of the water depth of the ocean variable fields in meters, e.g., -2000 and 0.
-* `theta_min` and `theta_max`: The minimum and maximum values of the latitude of the ocean variable field in degrees, e.g. -20 and 60.
-* `phi_min` and `phi_max`: The minimum and maximum values of the longitude of the ocean variable field in degrees, e.g. -120 and 170.
-* `t_min` and `t_max`: Minimum and maximum values of the ocean variable field time in the following format 'YYYY-MM-DDTHH:MM:SSZ', e.g. '2021-05-18T05:42:37Z'.
-* `train_vali_test`: The ratio of the training set, validation set, and test set, e.g. [8, 1, 1].
-
-## Train MDRF-Net
-
-Above is the structure of MDRF-Net, run the `Train.py` file to train the model.
-```
-python Train.py --data_path Data --batch_size 512 --activation tanh --initializer Glorot uniform --model_save_path_1 Code/Model1 --model_save_path_2 Code/Model2 --variable_save_path Data/variable.dat --learning_rate_1 1e-4 --learning_rate_2 1e-5
-```
-
-Command-line arguments explained:
-* `data_path`: Path to data storage.
-* `r_min` and `r_max`: The minimum and maximum values of the water depth of the ocean variable fields in meters, e.g., -2000 and 0.
-* `theta_min` and `theta_max`: The minimum and maximum values of the latitude of the ocean variable field in degrees, e.g. -20 and 60.
-* `phi_min` and `phi_max`: The minimum and maximum values of the longitude of the ocean variable field in degrees, e.g. -120 and 170.
-* `t_min` and `t_max`: Minimum and maximum values of the ocean variable field time in the following format 'YYYY-MM-DDTHH:MM:SSZ', e.g. '2021-05-18T05:42:37Z'.
-* `r_num`, `theta_num` and `phi_num`: The density of sampling points used for calculating equation errors across water depth, latitude, and longitude.
-* `batch_size`: The batch size for each variable's observed data points.
-* `init_beta_tau` and `init_beta_sigma`: The initial values of the parameters $\beta_\tau$ and $\beta_\sigma$ in the primitive equations.
-* `num_domain` and `num_boundary`: The number of sampling points on the domain and along the boundary used for computing the equation error.
-* `input_output_size` The number of inputs and outputs of the network.
-* `n_layers`: Each parallel subnet's number of layers.
-* `activation`: Activation function.
-* `initializer`: Parameters initialize method.
-* `model_save_path_1` and `model_save_path_2`: The storage paths for models in the two training phases.
-* `variable_save_path`: The storage path for the model's unknown parameters.
-* `save_period`: The storage interval for the model and its parameters, measured in iterations.
-* `resample_period_1` and `resample_period_2`: The resampling interval for observational data and sampling points, measured in iterations.
-* `num_iter_1` and `num_iter_2`: The total number of iterations for each stage in the two-stage training separately.
-* `optimizer`: Optimization algorithm.
-* `learning_rate_1` and `learning_rate_2`: The initial learning rates for the two training stages.
-* `loss_weights_1` and `loss_weights_2`: The internal weights of the loss function for the two training stages.
-
-
-# R Shiny platform
+## R Shiny platform
 
 We have integrated all the variable prediction performances across different times and spaces into an interactive R Shiny platform (https://tikitakatikitaka.shinyapps.io/mdrf-net-shiny/). This platform facilitates users to access information about marine variables of interest across different time and spatial dimensions.
 
--- Simulation.py
+++ Simulation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 from Code.Model import SimulationModel
 from Code.Plot import SimulationPlot
 from Code.utils import unpack_x_sim
@@ -16,40 +17,40 @@ ZETA_TAU = 0.02
 # -------------------- Data generation --------------------
 
 # Generatie simulation data
-# data_loader = SimulationData(eta = ETA, zeta = ZETA, zeta_tau = ZETA_TAU)
-# data_loader.gen_simulate_data(num=1000, noise_std=0, simulation_data_path = 'Data/Simulation/Train')
-# data_loader.gen_simulate_data(num=250, noise_std=0, simulation_data_path = 'Data/Simulation/Validation')
+data_loader = SimulationData(eta = ETA, zeta = ZETA, zeta_tau = ZETA_TAU)
+data_loader.gen_simulate_data(num=1000, noise_std=0, simulation_data_path = 'Data/Simulation/Train')
+data_loader.gen_simulate_data(num=250, noise_std=0, simulation_data_path = 'Data/Simulation/Validation')
 
 # Plot data
-# plotter = SimulationPlot()
-# plotter.simulation_data_plot(simulation_data_path = 'Data/Simulation/Train', save_path='Output/Plot/Simulation/Data/Train')
-# plotter.simulation_data_plot(simulation_data_path = 'Data/Simulation/Validation', save_path='Output/Plot/Simulation/Data/Validation')
+plotter = SimulationPlot()
+plotter.simulation_data_plot(simulation_data_path = 'Data/Simulation/Train', save_path='Output/Plot/Simulation/Data/Train')
+plotter.simulation_data_plot(simulation_data_path = 'Data/Simulation/Validation', save_path='Output/Plot/Simulation/Data/Validation')
 
 # -------------------- Coordinates generation --------------------
 
 # Generate new coordinates for test
-# simulation_gen_new_x(density=101, density_t=101, save_path='Data/simulation_new_x.npy')
+data_loader.simulation_gen_new_x(density=101, density_t=101, save_path='Data/simulation_new_x.npy')
 
 # Generate new coordinates for plot
-# simulation_gen_new_x(density=1001, density_t=5, save_path='Data/Simulation/simulation_new_x_plot.npy')
+data_loader.simulation_gen_new_x(density=1001, density_t=5, save_path='Data/Simulation/simulation_new_x_plot.npy')
 
 # -------------------- Solutions generation --------------------
 
 # Generate solution for test
-# new_x = np.load('Data/Simulation/Coordinates/simulation_new_x.npy')
-# x, z, t = unpack_x_sim(new_x)
-# taylor_green_vortex(x, z, t, eta = ETA, zeta = ZETA, zeta_tau = ZETA_TAU, save=True, save_path='Data/Simulation/Solutions/simulation_solution.npy')
+new_x = np.load('Data/Simulation/Coordinates/simulation_new_x.npy')
+x, z, t = unpack_x_sim(new_x)
+data_loader.taylor_green_vortex(x, z, t, eta = ETA, zeta = ZETA, zeta_tau = ZETA_TAU, save=True, save_path='Data/Simulation/Solutions/simulation_solution.npy')
 
 # Generate solution for plot
-# new_x_plot = np.load('Data/Simulation/Coordinates/simulation_new_x_plot.npy')
-# x, z, t = unpack_x_sim(new_x_plot)
-# taylor_green_vortex(x, z, t, eta = ETA, zeta = ZETA, zeta_tau = ZETA_TAU, save=True, save_path='Data/Simulation/Solutions/simulation_solution_plot.npy')
+new_x_plot = np.load('Data/Simulation/Coordinates/simulation_new_x_plot.npy')
+x, z, t = unpack_x_sim(new_x_plot)
+data_loader.taylor_green_vortex(x, z, t, eta = ETA, zeta = ZETA, zeta_tau = ZETA_TAU, save=True, save_path='Data/Simulation/Solutions/simulation_solution_plot.npy')
 
 # Plot solution
-# simulation_plot(
-#     X_path='Data/Simulation/Coordinates/simulation_new_x_plot.npy', 
-#     Y_path='Data/Simulation/Solutions/simulation_solution_plot.npy', 
-#     save_path='Output/Plot/Simulation/Solution')
+plotter.simulation_plot(
+    X_path='Data/Simulation/Coordinates/simulation_new_x_plot.npy', 
+    Y_path='Data/Simulation/Solutions/simulation_solution_plot.npy', 
+    save_path='Output/Plot/Simulation/Solution')
 
 
 ######################### MAD-REF net (Dynamic Loss Weight) #########################
@@ -66,30 +67,30 @@ mdrf = SimulationModel(task_name=TASK_NAME,
                        hard_constraint=True, 
                        meta_learning=True, 
                        output_dir=f'Output/Model/Simulation/{TASK_NAME}')
-# mdrf.simulation_train(simulation_data_path='Data/Simulation', 
-#                       iters=ITERS, 
-#                       display=100, 
-#                       save_period=1000, 
-#                       lrs=[1e-4, 1e-5], 
-#                       loss_weight=[1] * 4 + [1] * 3, 
-#                       frozen=False)
+mdrf.simulation_train(simulation_data_path='Data/Simulation', 
+                      iters=ITERS, 
+                      display=100, 
+                      save_period=1000, 
+                      lrs=[1e-4, 1e-5], 
+                      loss_weight=[1] * 4 + [1] * 3, 
+                      frozen=False)
 
-# Train 10 times
-# for i in tqdm(range(10)):
-#     mdrf.simulation_train(simulation_data_path='Data/Simulation/Train', output_dir=f'Output/Model/Simulation/{TASK_NAME}', iters=ITERS, display=100, lrs=LRS, variable_file_name=f'variable_{i}.dat')
+# Train 100 times
+for i in tqdm(range(100)):
+    mdrf.simulation_train(simulation_data_path='Data/Simulation/Train', output_dir=f'Output/Model/Simulation/{TASK_NAME}', iters=ITERS, display=100, lrs=LRS, variable_file_name=f'variable_{i}.dat')
 
 # -------------------- Prediction --------------------
 
 # Prediction for plot
-# mdrf.simulation_inference_steps(
-#     model_num_1 = ITERS[0],
-#     model_num_2 = ITERS[1], 
-#     checkpoint=f'Output/Model/Simulation/{TASK_NAME}', 
-#     new_x_path='Data/Simulation/Coordinates/simulation_new_x_plot.npy', 
-#     save_path=f'Output/Prediction/Simulation/{TASK_NAME}/prediction_plot',
-#     para=2)
+mdrf.simulation_inference_steps(
+    model_num_1 = ITERS[0],
+    model_num_2 = ITERS[1], 
+    checkpoint=f'Output/Model/Simulation/{TASK_NAME}', 
+    new_x_path='Data/Simulation/Coordinates/simulation_new_x_plot.npy', 
+    save_path=f'Output/Prediction/Simulation/{TASK_NAME}/prediction_plot',
+    para=2)
 
-# mdrf.rmse_trend()
+mdrf.rmse_trend()
 
 
 # -------------------- Plot --------------------
@@ -97,126 +98,28 @@ mdrf = SimulationModel(task_name=TASK_NAME,
 mdrf_plot = SimulationPlot()
 
 # Plot prediction
-# mdrf_plot.simulation_plot_2steps(
-#     X_path='Data/Simulation/Coordinates/simulation_new_x_plot.npy', 
-#     Y_path=f'Output/Prediction/Simulation/{TASK_NAME}/prediction_plot', 
-#     save_path=f'Output/Plot/Simulation/Prediction/{TASK_NAME}',
-#     plot_step_1=True)
+mdrf_plot.simulation_plot_2steps(
+    X_path='Data/Simulation/Coordinates/simulation_new_x_plot.npy', 
+    Y_path=f'Output/Prediction/Simulation/{TASK_NAME}/prediction_plot', 
+    save_path=f'Output/Plot/Simulation/Prediction/{TASK_NAME}',
+    plot_step_1=True)
 
-# simulation_plot_res(
-#     X_path='Data/Simulation/Coordinates/simulation_new_x_plot.npy', 
-#     pre_path=f'Output/Prediction/Simulation/{TASK_NAME}/prediction_plot_step2.npy', 
-#     sol_path='Data/Simulation/Solutions/simulation_solution_plot.npy', 
-#     save_path=f'Output/Plot/Simulation/Prediction/{TASK_NAME}Res')
+plotter.simulation_plot_res(
+    X_path='Data/Simulation/Coordinates/simulation_new_x_plot.npy', 
+    pre_path=f'Output/Prediction/Simulation/{TASK_NAME}/prediction_plot_step2.npy', 
+    sol_path='Data/Simulation/Solutions/simulation_solution_plot.npy', 
+    save_path=f'Output/Plot/Simulation/Prediction/{TASK_NAME}Res')
 
-# simulation_plot_parameters(model_dir=f'Output/Model/Simulation/{TASK_NAME}', save_dir='Output/Plot/Simulation/parameters.png')
+plotter.simulation_plot_parameters(model_dir=f'Output/Model/Simulation/{TASK_NAME}', save_dir='Output/Plot/Simulation/parameters.png')
 
-# simulation_plot_rmse_2(
-#     sol_path='Data/Simulation/Solutions/simulation_solution_plot.npy', 
-#     mdrf_path=f'Output/Prediction/Simulation/{TASK_NAME}/prediction_plot_step2.npy',
-#     nmdrf_path='Output/Prediction/Simulation/NoPhysics/prediction_plot.npy',
-#     gpr_path='Output/Prediction/Simulation/GPR/prediction_plot.npy',
-#     kriging_path='Output/Prediction/Simulation/Kriging/prediction_plot.npy',
-#     save_path=f'Output/Plot/Simulation/simualtion_rmse_{TASK_NAME}.png')
+plotter.simulation_plot_rmse_2(
+    sol_path='Data/Simulation/Solutions/simulation_solution_plot.npy', 
+    mdrf_path=f'Output/Prediction/Simulation/{TASK_NAME}/prediction_plot_step2.npy',
+    nmdrf_path='Output/Prediction/Simulation/NoPhysics/prediction_plot.npy',
+    gpr_path='Output/Prediction/Simulation/GPR/prediction_plot.npy',
+    kriging_path='Output/Prediction/Simulation/Kriging/prediction_plot.npy',
+    save_path=f'Output/Plot/Simulation/simualtion_rmse_{TASK_NAME}.png')
 
-# mdrf_plot.simlulation_plot_lambdas(file_path=f'Output/Model/Simulation/{TASK_NAME}/Step2/lambdas.csv', output_image_path=f'Output/Plot/Simulation/{TASK_NAME}_simualtion_lambdas.png')
-
-mdrf_plot.plot_rmse_trends(tasks=['2Steps_2Para', 
-                                  '2Steps_2Para_Meta', 
-                                  '2Steps_2Para_HardConPeriodic', 
-                                  '2Steps_2Para_HardConPeriodic_MetaMin'], 
-                           task_names=['None', 
-                                       'Meta Learning', 
-                                       'Hard Constraint', 
-                                       'Hard Constraint + Meta Learning'], 
-                           output_path='Output/Plot/Simulation/rmse_trends_average.png',
-                           average_tasks=True, 
-)
-
-######################### No Physics #########################
-
-# -------------------- Training --------------------
-
-# simulation_train_no_physics(simulation_data_path='Data/Simulation/Train', output_dir='Output/Model/Simulation/NoPhysics', iters=15000, display=100, lrs=1e-4)
-
-# -------------------- Prediction --------------------
-
-# Prediction for plot
-# simulation_inference(
-#     checkpoint='Output/Model/Simulation/NoPhysics/model-10000.pt', 
-#     new_x_path='Data/Simulation/Coordinates/simulation_new_x_plot.npy', 
-#     save_path='Output/Prediction/Simulation/NoPhysics/prediction_plot.npy')
-
-# -------------------- Plot --------------------
-
-# Plot prediction
-# simulation_plot(
-#     X_path='Data/Simulation/Coordinates/simulation_new_x_plot.npy', 
-#     Y_path='Output/Prediction/Simulation/NoPhysics/prediction_plot.npy', 
-#     save_path='Output/Plot/Simulation/Prediction/NoPhysics')
-
-# simulation_plot_res(
-#     X_path='Data/Simulation/Coordinates/simulation_new_x_plot.npy', 
-#     pre_path='Output/Prediction/Simulation/NoPhysics/prediction_plot.npy', 
-#     sol_path='Data/Simulation/Solutions/simulation_solution_plot.npy', 
-#     save_path='Output/Plot/Simulation/Prediction/NoPhysicsRes')
-
-
-######################### Kriging #########################
-
-# -------------------- Prediction --------------------
-
-# simulation_other_methods_inference(
-#     simulation_data_path='Data/Simulation/Train', 
-#     new_x_path='Data/Simulation/Coordinates/simulation_new_x_plot.npy', 
-#     save_path='Output/Prediction/Simulation/Kriging/prediction_plot.npy',
-#     model='kriging')
-
-# -------------------- Plot --------------------
-
-# Plot kriging results
-# simulation_plot(X_path='Data/Simulation/Coordinates/simulation_new_x_plot.npy', Y_path='Output/Prediction/Simulation/Kriging/prediction_plot.npy', save_path='Output/Plot/Simulation/Prediction/Kriging')
-
-
-######################### GPR #########################
-
-# -------------------- Prediction --------------------
-
-# Gaussian preocess regression
-# simulation_other_methods_inference(
-#     simulation_data_path='Data/Simulation/Train', 
-#     new_x_path='Data/Simulation/Coordinates/simulation_new_x_plot.npy', 
-#     save_path='Output/Prediction/Simulation/GPR/prediction_plot.npy',
-#     model='gpr')
-
-# -------------------- Plot --------------------
-
-# Plot GPR
-# simulation_plot(X_path='Data/Simulation/Coordinates/simulation_new_x_plot.npy', Y_path='Output/Prediction/Simulation/GPR/prediction_plot.npy', save_path='Output/Plot/Simulation/Prediction/GPR')
-
-
-######################### Modelling #########################
-
-
-
-# -------------------- Prediction --------------------
-
-# Prediction for test
-# simulation_inference(checkpoint='Output/Model/Simulation/Step2/model-30000.pt', new_x_path='Data/Simulation/simulation_new_x.npy', save_path='Output/Prediction/Simulation_2steps/prediction.npy')
-
-
-
-
-
-# -------------------- Testing --------------------
-
-# solution = np.load('Data/Simulation/simulation_solution.npy')
-# prediction = np.load('Output/Prediction/Simulation_2steps/prediction.npy')
-# print('Simulation RMSE:', np.sqrt(mean_squared_error(solution, prediction)))
-
-
-######################### Plot #########################
-
-
+mdrf_plot.simlulation_plot_lambdas(file_path=f'Output/Model/Simulation/{TASK_NAME}/Step2/lambdas.csv', output_image_path=f'Output/Plot/Simulation/{TASK_NAME}_simualtion_lambdas.png')
 
 
